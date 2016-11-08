5b08d8a5ddbc0c2ec0d5dd718751cdf1fe30d511
slovohrista <harusan@MacBook-Air-Volodymyr.local>
2016-11-09 00:35:49 +0200

    upd new post page

--- js/new_app.js
+++ js/new_app.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", function(event)  {
+document.addEventListener("DOMContentLoaded", function(event) {
+  var filename, startBlock = "";
 
     $(".gen").click(function() {
         // var title = document.getElementById("title").value;
@@ -9,15 +10,35 @@ document.addEventListener("DOMContentLoaded", function(event)  {
         date = $(".date").val();
 
         if (document.getElementById("doctrine").checked) {
-          category = "doctrine";
+            category = "doctrine";
         } else if (document.getElementById("poetry").checked) {
-          category = "poetry";
+            category = "poetry";
         }
 
         startBlock = "---\nlayout: single\ncategory: " + category + "\ntitle: " + title + "\nauthor: " + author + "\n---";
+        filename = date + "-" + url_slug(title) + ".md";
 
         $(".start-block").html(startBlock);
-        $(".file-block").html(date + "-" + url_slug(title) + ".md");
+        $(".file-block").html(filename);
+    });
+
+    function download(filename, text) {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+
+        element.style.display = 'none';
+        document.body.appendChild(element);
+
+        element.click();
+
+        document.body.removeChild(element);
+    }
+
+    $(".download").click(function() {
+      console.log("download");
+        // Start file download.
+        download(filename, startBlock);
     });
 
 
